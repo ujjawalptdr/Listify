608881d1dc9610dc0ffa745433da54e6542c23f9
ujjawalptdr <ujjawalpatidar0303@gmail.com>
2024-10-29 11:12:22 +0530

    adding style and textArea

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-const submit = document.querySelector(".form-todo")
-const inputText = document.querySelector(".form-todo input[type='text']")
-const ul = document.querySelector(".todoList")
+const submit = document.querySelector(".form-todo");
+const inputText = document.querySelector(".form-todo textarea");
+const ul = document.querySelector(".todoList");
 
-submit.addEventListener('submit',(e)=>{
-    e.preventDefault();
+submit.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    let todoHeading = inputText.value;
+  let todoHeading = inputText.value;
 
-    if(todoHeading != ""){
-        const newLi = document.createElement("li");
+  if (todoHeading != "") {
+    const newLi = document.createElement("li");
 
-        newLi.innerHTML = `<span class="text">${todoHeading}</span>  
+    newLi.innerHTML = `<span class="text">${todoHeading}</span>  
         <div class = "div-todo">
         <button class="btn btn-todo todo-done">Done</button><button class="btn btn-todo todo-remove">Remove</button>
-        </div>`
+        </div>`;
 
-        ul.append(newLi);
-        inputText.value = "";
-    }
-    
-})
+    ul.append(newLi);
+    inputText.value = "";
+  }
+});
 
-ul.addEventListener('click',(e)=>{
+ul.addEventListener("click", (e) => {
+  if (e.target.classList.contains("todo-done")) {
+    e.target.parentNode.previousElementSibling.style.textDecoration =
+      "line-through";
+  }
 
-    if(e.target.classList.contains("todo-done")){
-        e.target.parentNode.previousElementSibling.style.textDecoration = "line-through";
-    }
-
-    if(e.target.classList.contains("todo-remove")){
-        e.target.parentNode.parentNode.remove();
-    }
-})
+  if (e.target.classList.contains("todo-remove")) {
+    e.target.parentNode.parentNode.remove();
+  }
+});
